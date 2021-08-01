ea89866b307b8864728fd322be75dad0a06f16a0
Unnam Pearly Susan <unnam.jessica@gmail.com>
2021-08-01 13:02:50 +0530

    first commit

--- convertion.py
+++ convertion.py
@@ -1,3 +1,5 @@
+
+
 import json
 from flask import Flask, render_template, request, jsonify   
 
@@ -22,57 +24,80 @@ def sum3():
 @app.route('/sum4')
 def sum4():    
     return render_template('sum4.html')
-   
+
+def ans1(a,b):
+    list2=[]
+    ans=""
+    for i in a:
+        if i not in list2:
+            list2.append(i)
+    list3=[0]*len(list2)
+    for i in range (len(list2)):
+        for j in a:
+            if j==list2[i]:
+                list3[i]=list3[i]+1
+    for k in range (len(list3)):
+        if list3[k]>2:
+            ans=ans+"<b>Item "+str(list2[k])+" with frequency "+str(list3[k])+"</b><br>"
+    return ans
+
 @app.route("/submitJSON1", methods=["POST"])
 def processJSON1(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
     
     response = ""
+
     l=jsonObj['l'].split(',')
     k=int(jsonObj['k'])
-    list1=[]
-    list3=[]
-##unique
-    for i in l:
-        if i not in list1:
-            list1.append(i)
-    list2=[0]*len(list1)
-    for i in range (len(list1)):
-        for j in range (len(l)):
-            if list1[i]==l[j]:
-                list2[i]=list2[i]+1
-    for i in range (len(list2)):
-        if (k<list2[i]):
-             list3.append(list1[i])         
-    response+="<b> "+str(list3)+"</b><br>"
-        
-    
-    	    
+    response=ans1(l,k)	    
     return response
 
+
+def ans2(a):
+    list2=[]
+    for i in a:
+        if i not in list2:
+            list2.append(i)
+    list3=[0]*len(list2)
+    for i in range (len(list2)):
+        for j in a:
+            if j==list2[i]:
+                list3[i]=list3[i]+1
+    for k in range (len(list3)):
+        if list3[k]>1:
+            temp=list2[k]
+            for i in range(list3[k]-1):
+                a.remove(temp)
 @app.route("/submitJSON2", methods=["POST"])
 def processJSON2(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
     
     response = ""
-    l1=jsonObj['l1'].split(',')
-    l2=jsonObj['l2'].split(',')
-    k=l2.reverse()
-    m=min(len(l1),len(l2))
-    p=max(len(l1),len(l2))
-    for i in range (m):
-        response+="<b> "+str(l1[i])+" " +str(l2[i])+"</b><br>"
-    if (len(l1)==p):
-        for i in range (-(p-m),0):
-            response+="<b> "+str(l1[i])+"</b><br>"
-    elif (len(l2)==p):
-        for i in range (-(p-m),0):
-            response+="<b> "+str(l2[i])+"</b><br>"
-    	    
+    l=jsonObj['l'].split(',')
+    ans2(l)
+    response=str(l)    	    
     return response
 
+def ans3(a,row,col):
+    ans=""
+    flag=True
+    if (row!=col):
+        flag=False
+    for i in range (col):
+        for j in range (row):
+            if (col==row):
+                if (a[j][i]!=a[i][j] and flag):
+                    flag=False
+            ans=ans+a[j][i]+" "
+        ans=ans+"<br>"
+    if (flag):
+        ans=ans+"Symmetfr5rdric"
+        return ans
+    else:
+        ans=ans+"Not Symmetric"
+        return ans
 @app.route("/submitJSON3", methods=["POST"])
 def processJSON3(): 
     jsonStr = request.get_json()
@@ -80,39 +105,35 @@ def processJSON3():
     
     response = ""
     l1=jsonObj['l1'].split(',')
-    list1=[]
-    for i in l1:
-        if i not in list1:
-            list1.append(i)
-    response+="<b> "+str(list1)+"</b><br>"
+    l2=int(jsonObj['l2'])
+    l3=int(jsonObj['l3'])
+    array=[]
+    k=0
+    for i in range (l2):
+        temp=[]
+        for j in range (l3):
+            temp.append(l1[k])
+            k=k+1
+        array.append(temp)
+    response=ans3(array,l2,l3)
     	    
     return response
 
+def ans4(a,ans):
+    if (a==1):
+        return "1"+ans
+    if (a%2==0):
+        return ans4(a//2,"0"+ans)
+    else:
+        return ans4(a//2,"1"+ans)
 @app.route("/submitJSON4", methods=["POST"])
-#two lists and for loops
-
 def processJSON4(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
-    
-    response = ""
-    l1=jsonObj['l1'].split(',')
-    l2=jsonObj['l2'].split(',')
-    ans=[]
-    for i in range (len(l1)):
-        l1[i]=l1[i].lower()
-    for i in range (len(l2)):
-        l2[i]=l2[i].lower()
-    for i in l2:
-        if i in l1 and i not in ans:
-            ans.append(i)
-
-    response+="<b> "+str(ans)+"</b><br>"
-    	    
+    i=int(jsonObj['i'])
+    response=ans4(i,"")
     return response
     
-
-
 if __name__ == "__main__":
     app.run(debug=True)
     
