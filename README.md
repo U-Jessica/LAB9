47113160a1b955270583e73b23b419b54e976f97
Unnam Pearly Susan <unnam.jessica@gmail.com>
2021-08-08 16:42:41 +0530

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 # LAB-4
 # LAB5
+# LAB6
+# LAB6
--- convertion.py
+++ convertion.py
@@ -25,23 +25,11 @@ def sum3():
 def sum4():    
     return render_template('sum4.html')
 
-def ans1(a,b):
-    list2=[]
-    ans=""
-    for i in a:
-        if i not in list2:
-            list2.append(i)
-    list3=[0]*len(list2)
-    for i in range (len(list2)):
-        for j in a:
-            if j==list2[i]:
-                list3[i]=list3[i]+1
-    for k in range (b):
-        index=list3.index(max(list3))
-        ans=ans+"<b>Item "+str(list2[index])+" with frequency "+str(list3[index])+"</b><br>"
-        del list3[index]
-        del list2[index]    
-    return ans
+@app.route('/sum5')
+def sum5():    
+    return render_template('sum5.html')
+
+
 
 @app.route("/submitJSON1", methods=["POST"])
 def processJSON1(): 
@@ -50,90 +38,101 @@ def processJSON1():
     
     response = ""
 
-    l=jsonObj['l'].split(',')
-    k=int(jsonObj['k'])
-    response=ans1(l,k)	    
+    l=jsonObj['l'].split(';')
+    for i in range (len(l)):
+        if (l[i]==''):
+            l[i]=tuple()
+            continue
+        l[i]=tuple(map(int,l[i].split(",")))
+    while(() in l):
+        l.remove(())
+    response=str(l)
     return response
 
 
-def ans2(a):
-    list2=[]
-    for i in a:
-        if i not in list2:
-            list2.append(i)
-    list3=[0]*len(list2)
-    for i in range (len(list2)):
-        for j in a:
-            if j==list2[i]:
-                list3[i]=list3[i]+1
-    for k in range (len(list3)):
-        if list3[k]>1:
-            temp=list2[k]
-            for i in range(list3[k]-1):
-                a.remove(temp)
+def test(lst):
+    result=map(sum,lst)
+    return list(result)
+
+
 @app.route("/submitJSON2", methods=["POST"])
 def processJSON2(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
     
     response = ""
-    l=jsonObj['l'].split(',')
-    ans2(l)
-    response=str(l)    	    
+    l=jsonObj['l'].split(';')
+    for i in range (len(l)):
+        if(l[i]==''):
+            l[i]=tuple()
+            continue
+        l[i]=tuple(map(int,l[i].split(",")))
+    response=str(test(l))    	    
     return response
 
-def ans3(a,row,col):
-    ans=""
-    flag=True
-    if (row!=col):
-        flag=False
-    for i in range (col):
-        for j in range (row):
-            if (col==row):
-                if (a[j][i]!=a[i][j] and flag):
-                    flag=False
-            ans=ans+a[j][i]+" "
-        ans=ans+"<br>"
-    if (flag):
-        ans=ans+"Symmetfr5rdric"
-        return ans
-    else:
-        ans=ans+"Not Symmetric"
-        return ans
+
 @app.route("/submitJSON3", methods=["POST"])
 def processJSON3(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
     
     response = ""
-    l1=jsonObj['l1'].split(',')
-    l2=int(jsonObj['l2'])
-    l3=int(jsonObj['l3'])
-    array=[]
-    k=0
-    for i in range (l2):
-        temp=[]
-        for j in range (l3):
-            temp.append(l1[k])
-            k=k+1
-        array.append(temp)
-    response=ans3(array,l2,l3)
+    l1=jsonObj['l1'].split(';')
+    l2=jsonObj['l2'].split(',')
+    test_dict={}
+    for i in l1:
+        a=i.split(",")
+        test_dict[a[0]]=a[1]
+    keys=list(test_dict.keys())
+    for i in keys:
+        for j in l2:
+            if test_dict[i].find(j) != -1:
+                del test_dict[i]
+                break
+    response=str(test_dict)
     	    
     return response
 
-def ans4(a,ans):
-    if (a==1):
-        return "1"+ans
-    if (a%2==0):
-        return ans4(a//2,"0"+ans)
-    else:
-        return ans4(a//2,"1"+ans)
+
 @app.route("/submitJSON4", methods=["POST"])
 def processJSON4(): 
     jsonStr = request.get_json()
     jsonObj = json.loads(jsonStr) 
-    i=int(jsonObj['i'])
-    response=ans4(i,"")
+    
+    response = ""
+    l=list(map(int,jsonObj['l'].split(',')))
+    counts=dict()
+    for i in range(1,(max(l)+1)):
+        counts[i]=0
+        for key in l:
+            if key%i==0:
+                counts[i]=counts[i]+1
+    response=str(counts)    	    
+    return response
+@app.route("/submitJSON5", methods=["POST"])
+def processJSON5(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr) 
+    
+    response = ""
+    f={'music':{'x':5,'y':6,'z':3},'best':{'x':8,'y':3,'z':5}}
+    dod={}
+    ll=[]
+    for v in f.values():
+        for kk in v.keys():
+            dod[kk]=[]
+    for k,v in f.items():
+        for kk,vv in v.items():
+            dod[kk].append(tuple((k,vv)))
+
+    response=str(dod)
+    for k,v in dod.items():
+        temp=[]
+        temp.append(k)
+        for vv in v:
+            temp.append(vv)
+        ll.append(tuple(temp))
+    response=str(tuple(ll))     
     return response
     
 if __name__ == "__main__":
